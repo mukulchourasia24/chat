bb33a3e27edadf9bdc3b269de0ae0c51bc532d42
mkkulchourasia24 <mukulch24@gmail.com>
2020-09-20 11:54:55 +0530

    second commit

--- lib/chat_web/channels/water_cooler_channel.ex
+++ lib/chat_web/channels/water_cooler_channel.ex
@@ -5,6 +5,7 @@ defmodule ChatWeb.WaterCoolerChannel do
     {:ok, socket}
   end
 
+
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
 #  def handle_in("ping", payload, socket) do
